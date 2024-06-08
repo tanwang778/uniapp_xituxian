87390fc4edf27d778452f0e07d13625d1d723bc9
战神谭 <2764732335@qq.com>
2024-06-08 15:16:42 +0800

    前台分类组件的封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { getHomeBannerAPI } from '@/services/home'
 import CustomNavBar from './components/CustomNavBar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import type { BannerItem } from '@/types/home'
@@ -19,7 +20,13 @@ onLoad(() => {
   <!-- 自定义导航栏 -->
   <CustomNavBar />
   <!-- 自定义轮播图 -->
-  <XtxSwiper :bannerList="bannerList" />
+  <XtxSwiper :bannerList="bannerList!" />
+  <!-- 分类 -->
+  <CategoryPanel />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+}
+</style>
