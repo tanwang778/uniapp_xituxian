a79ae37a24842b6ee1fd253a4b26891f4b248cc9
战神谭 <2764732335@qq.com>
2024-06-11 10:03:17 +0800

    热门推荐准备工作

--- src/pages/index/components/HotPanel.vue
+++ src/pages/index/components/HotPanel.vue
@@ -14,7 +14,7 @@ defineProps<{
         <text class="title-text">{{ item.title }}</text>
         <text class="title-desc">{{ item.alt }}</text>
       </view>
-      <navigator hover-class="none" url="/page/recommend/recommend" class="cards">
+      <navigator hover-class="none" :url="`/pages/hot/hot?type=${item.type}`" class="cards">
         <image
           v-for="src in item.pictures"
           :key="src"
